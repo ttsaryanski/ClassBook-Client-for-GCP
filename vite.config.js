e3364d64c5bfbea5135397a1b0e00b6ba776e0c0
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2025-06-23 17:45:54 +0300

    Add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "ClassBook App",
+                short_name: "ClassBook",
+                start_url: "/",
+                display: "standalone",
+                background_color: "#ffffff",
+                theme_color: "#2c3e50",
+            },
+        }),
+    ],
+    base: "/",
+    server: {
+        historyApiFallback: true,
+    },
 });
